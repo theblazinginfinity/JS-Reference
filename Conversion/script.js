2b632dcc3a5cef0a5cd872ce9c01524b7b7cfeb1
Kendric Lin <127161680+theblazinginfinity@users.noreply.github.com>
2024-01-11 18:29:26 -0800

    f
    f

--- Conversion/script.js
+++ Conversion/script.js
@@ -1,3 +1,17 @@
-function test(){
-    console.log("Hello from the script!");
+function convert(e) {
+    const elemID = e.id;
+    let degrees = document.getElementById(elemID).value;
+    console.log(`${degrees} input from ${elemID}`);
+    elemID == 'fahrInput' ? getCelsius() : getFahr();
+}
+
+function getCelsius() {
+    let f = pareFloat(d);
+    c = (5/9) * (f - 32);
+}
+
+function getFahr() {
+    let c = 0;
+    let f = parseFloat(d);
+    return c = (5/9) * (f - 32);
 }
